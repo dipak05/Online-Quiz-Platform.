9341a2fe993a3bc0d857035b0e08330f3b694ef6
dipak05 <33412169+dipak05@users.noreply.github.com>
2018-03-17 09:55:51 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -1,24 +1,23 @@
 <style type="text/css">
-<!--
+
 body {
 	margin-left: 0px;
 	margin-top: 0px;
 }
--->
+
 </style>
-<table border="0" width="100%" cellspacing="0" cellpadding="0" background="../image/topbkg.jpg">
+<table border="0" width="100%" cellspacing="0" cellpadding="0" background="image/topbkg.jpg">
   <tr>
     <td width="90%" valign="top">
-<!--You can modify the text, color, size, number of loops and more on the flash header by editing the text file (fence.txt).-->
-<div align="left"><object classid=clsid:D27CDB6E-AE6D-11cf-96B8-444553540000
+<div align="left"><object classid=clsid:D27CDB6E-AE6D-11cf-96B8-44455354000
 width=500
 height=68>
-<param name=movie value=../image/fence.swf>
+<param name=movie value=image/fence.swf>
 <param name=quality value=high>
 <param name=BGCOLOR value=#000000>
 <param name=SCALE value=showall>
 <param name=wmode value=transparent> 
-<embed src=../image/fence.swf
+<embed src=image/fence.swf
 quality=high
 width=500
 height=68
@@ -27,21 +26,26 @@ scale= showall>
 </embed>
 </object></div></td>
     <td width="10%">
-     <img border="0" src="../image/topright.jpg" width="203" height="68" align="right"></td>
+     <img border="0" src="image/topright.jpg" width="203" height="68" align="right"></td>
   </tr>
 </table>
-<table border="0" width="100%" cellspacing="0" cellpadding="0" bgcolor="#000000" background="../image/blackbar.jpg">
+<table border="0" width="100%" cellspacing="0" cellpadding="0" bgcolor="#000000" background="img/blackbar.jpg">
   <tr>
-    <td width="100%"><img border="0" src="../image/blackbar.jpg" width="89" height="15"></td>
+    <td width="100%" align="right"><img border="0" src="image/blackbar.jpg" width="89" height="15"></td>
   </tr>
-</table>
-  <table width="100%">
+  </Table>
+  <Table width="100%">
   <tr>
-    <td aling=right>
+  <td>
+  <?php @$_SESSION['login']; 
+  error_reporting(1);
+  ?>
+  </td>
+    <td>
 	<?php
-	if(isset($_SESSION['alogin']))
+	if(isset($_SESSION['login']))
 	{
-	 echo "<div align=\"right\"><strong><a href=\"login.php\">Admin Home</a>|<a href=\"signout.php\">Signout</a></strong></div>";
+	 echo "<div align=\"right\"><strong><a href=\"index.php\"> Home </a>|<a href=\"signout.php\">Signout</a></strong></div>";
 	 }
 	 else
 	 {
@@ -49,5 +53,7 @@ scale= showall>
 	 }
 	?>
 	</td>
+	
   </tr>
+  
 </table>
